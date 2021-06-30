75228fb666cb844ecf5edef995385666256f8f40
woodwardmw <mark@mark-laptop.home>
2021-06-30 14:04:20 +0100

    Added new scripts from EDX exercises

--- Baseball/baseballScripts.R
+++ Baseball/baseballScripts.R
@@ -17,3 +17,33 @@ Teams %>% filter(yearID %in% 1961:2001) %>% mutate(wins_per_game = W / G, E_per_
 
 Teams %>% filter(yearID %in% 1961:2001) %>% mutate(X3B_per_game = X3B / G, X2B_per_game = X2B / G) %>%
   ggplot(aes(X3B_per_game, X2B_per_game)) + geom_point()
+
+
+---------------------
+
+
+#find regression line for predicting runs from BBs
+library(tidyverse)
+library(Lahman)
+bb_slope <- Teams %>% 
+  filter(yearID %in% 1961:2001 ) %>% 
+  mutate(BB_per_game = BB/G, R_per_game = R/G) %>% 
+  lm(R_per_game ~ BB_per_game, data = .) %>% 
+  .$coef %>%
+  .[2]
+bb_slope
+
+# compute regression line for predicting runs from singles
+singles_slope <- Teams %>% 
+  filter(yearID %in% 1961:2001 ) %>%
+  mutate(Singles_per_game = (H-HR-X2B-X3B)/G, R_per_game = R/G) %>%
+  lm(R_per_game ~ Singles_per_game, data = .) %>%
+  .$coef  %>%
+  .[2]
+singles_slope
+
+# calculate correlation between HR, BB and singles
+Teams %>% 
+  filter(yearID %in% 1961:2001 ) %>% 
+  mutate(Singles = (H-HR-X2B-X3B)/G, BB = BB/G, HR = HR/G) %>%  
+  summarize(cor(BB, HR), cor(Singles, HR), cor(BB,Singles))
--- Galton-heights/GaltonScripts.R
+++ Galton-heights/GaltonScripts.R
@@ -35,3 +35,37 @@ galton_heights %>%
   ggplot() +  
   stat_qq(aes(sample = son)) +
   facet_wrap( ~ z_father)
+
+# compute a regression line to predict the son's height from the father's height
+mu_x <- mean(galton_heights$father)
+mu_y <- mean(galton_heights$son)
+s_x <- sd(galton_heights$father)
+s_y <- sd(galton_heights$son)
+r <- cor(galton_heights$father, galton_heights$son)
+m_1 <-  r * s_y / s_x
+b_1 <- mu_y - m_1*mu_x
+
+# compute a regression line to predict the father's height from the son's height
+m_2 <-  r * s_x / s_y
+b_2 <- mu_x - m_2*mu_y
+
+set.seed(1989, sample.kind="Rounding") #if you are using R 3.6 or later
+library(HistData)
+data("GaltonFamilies")
+
+female_heights <- GaltonFamilies%>%     
+  filter(gender == "female") %>%     
+  group_by(family) %>%     
+  sample_n(1) %>%     
+  ungroup() %>%     
+  select(mother, childHeight) %>%     
+  rename(daughter = childHeight)
+
+mean_mother <- mean(female_heights$mother)
+sd_mother <- sd(female_heights$mother)
+mean_daughter <- mean(female_heights$daughter)
+sd_daughter <- sd(female_heights$daughter)
+r <- cor(female_heights$mother, female_heights$daughter)
+slope <- r * sd_daughter / sd_mother
+intercept <- mean_daughter - slope * mean_mother
+
