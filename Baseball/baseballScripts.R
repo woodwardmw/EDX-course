c2f4b6508870184eae8906feed56eff9f31b1536
woodwardmw <mark@mark-laptop.home>
2021-06-30 21:19:33 +0100

    Added code corresponding to course exercise questions

--- Baseball/baseballScripts.R
+++ Baseball/baseballScripts.R
@@ -47,3 +47,74 @@ Teams %>%
   filter(yearID %in% 1961:2001 ) %>% 
   mutate(Singles = (H-HR-X2B-X3B)/G, BB = BB/G, HR = HR/G) %>%  
   summarize(cor(BB, HR), cor(Singles, HR), cor(BB,Singles))
+
+# stratify HR per game to nearest 10, filter out strata with few points
+dat <- Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(HR_strata = round(HR/G, 1), 
+         BB_per_game = BB / G,
+         R_per_game = R / G) %>%
+  filter(HR_strata >= 0.4 & HR_strata <=1.2)
+
+# scatterplot for each HR stratum
+dat %>% 
+  ggplot(aes(BB_per_game, R_per_game)) +  
+  geom_point(alpha = 0.5) +
+  geom_smooth(method = "lm") +
+  facet_wrap( ~ HR_strata)
+
+# calculate slope of regression line after stratifying by HR
+dat %>%  
+  group_by(HR_strata) %>%
+  summarize(slope = cor(BB_per_game, R_per_game)*sd(R_per_game)/sd(BB_per_game))
+
+# stratify by BB
+dat <- Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(BB_strata = round(BB/G, 1), 
+         HR_per_game = HR / G,
+         R_per_game = R / G) %>%
+  filter(BB_strata >= 2.8 & BB_strata <=3.9) 
+
+# scatterplot for each BB stratum
+dat %>% ggplot(aes(HR_per_game, R_per_game)) +  
+  geom_point(alpha = 0.5) +
+  geom_smooth(method = "lm") +
+  facet_wrap( ~ BB_strata)
+
+# slope of regression line after stratifying by BB
+dat %>%  
+  group_by(BB_strata) %>%
+  summarize(slope = cor(HR_per_game, R_per_game)*sd(R_per_game)/sd(HR_per_game)) 
+
+#Question 3
+library(Lahman)
+Teams <- Teams %>% filter(yearID %in% 1961:2001) %>% mutate(R_per_game = R / G, BB_per_game = BB / G, HR_per_game = HR / G)
+fit <- lm(R_per_game ~ BB_per_game + HR_per_game, data = Teams)
+summary(fit)
+fit$coefficients
+Teams %>% ggplot(aes(BB_per_game + HR_per_game, R_per_game)) + geom_point() + geom_smooth(method = "lm")
+
+
+#Question 9
+library(Lahman)
+bat_02 <- Batting %>% filter(yearID == 2002) %>%
+  mutate(pa = AB + BB, singles = (H - X2B - X3B - HR)/pa, bb = BB/pa) %>%
+  filter(pa >= 100) %>%
+  select(playerID, singles, bb)
+head(bat_02)
+
+bat_99_01 <- Batting %>% filter(yearID %in% 1999:2001) %>%
+  mutate(pa = AB + BB, singles = (H - X2B - X3B - HR)/pa, bb = BB/pa) %>%
+  filter(pa >= 100) %>% group_by(playerID) %>% summarise(mean_singles = mean(singles), mean_bb = mean(bb))
+head(bat_03)
+bat_99_01 %>% filter(mean_singles > 0.2) %>% nrow()
+bat_99_01 %>% filter(mean_bb > 0.2) %>% nrow()
+
+bat_99_02 <- inner_join(bat_02, bat_99_01)
+cor(bat_99_02$singles, bat_99_02$mean_singles)
+cor(bat_99_02$bb, bat_99_02$mean_bb)
+
+bat_99_02 %>% ggplot(aes(singles, mean_singles)) + geom_point()
+bat_99_02 %>% ggplot(aes(bb, mean_bb)) + geom_point()
+
+lm(singles ~ mean_singles, data = bat_99_02)
+lm(bb ~ mean_bb, data = bat_99_02)
--- Galton-heights/GaltonScripts.R
+++ Galton-heights/GaltonScripts.R
@@ -69,3 +69,117 @@ r <- cor(female_heights$mother, female_heights$daughter)
 slope <- r * sd_daughter / sd_mother
 intercept <- mean_daughter - slope * mean_mother
 
+
+lm(son ~ father, data = galton_heights)
+
+rss <- function(beta0, beta1){
+  resid <- galton_heights$son - (beta0+beta1*galton_heights$father)
+  return(sum(resid^2))
+}
+
+# plot RSS as a function of beta1 when beta0=25
+beta1 = seq(0, 1, len=nrow(galton_heights))
+results <- data.frame(beta1 = beta1,
+                      rss = sapply(beta1, rss, beta0 = 25))
+results %>% ggplot(aes(beta1, rss)) + geom_line() + 
+  geom_line(aes(beta1, rss))
+
+fit <- lm(son ~ father, data = galton_heights)
+summary(fit)
+
+# Monte Carlo simulation
+B <- 1000
+N <- 50
+lse <- replicate(B, {
+  sample_n(galton_heights, N, replace = TRUE) %>% 
+    lm(son ~ father, data = .) %>% 
+    .$coef 
+})
+lse <- data.frame(beta_0 = lse[1,], beta_1 = lse[2,]) 
+
+# Plot the distribution of beta_0 and beta_1
+library(gridExtra)
+p1 <- lse %>% ggplot(aes(beta_0)) + geom_histogram(binwidth = 5, color = "black") 
+p2 <- lse %>% ggplot(aes(beta_1)) + geom_histogram(binwidth = 0.1, color = "black") 
+grid.arrange(p1, p2, ncol = 2)
+
+# summary statistics
+sample_n(galton_heights, N, replace = TRUE) %>% 
+  lm(son ~ father, data = .) %>% 
+  summary %>%
+  .$coef
+
+lse %>% summarize(se_0 = sd(beta_0), se_1 = sd(beta_1))
+
+
+# But standardising the fathers heights gives a different correlation coefficient:
+B <- 1000
+N <- 50
+lse <- replicate(B, {
+  sample_n(galton_heights, N, replace = TRUE) %>%
+    mutate(father = father - mean(father)) %>%
+    lm(son ~ father, data = .) %>% .$coef 
+})
+
+cor(lse[1,], lse[2,]) 
+
+# plot predictions and confidence intervals
+galton_heights %>% ggplot(aes(father, son)) +
+  geom_point() +
+  geom_smooth(method = "lm")
+
+# predict Y directly
+fit <- galton_heights %>% lm(son ~ father, data = .) 
+Y_hat <- predict(fit, se.fit = TRUE)
+names(Y_hat)
+
+# plot best fit line
+galton_heights %>%
+  mutate(Y_hat = predict(lm(son ~ father, data=.))) %>%
+  ggplot(aes(father, Y_hat))+
+  geom_line()
+
+#Question 1
+beta1 = seq(0, 1, len=nrow(galton_heights))
+results <- data.frame(beta1 = beta1,
+                      rss = sapply(beta1, rss, beta0 = 36))
+results %>% ggplot(aes(beta1, rss)) + geom_line() + 
+  geom_line(aes(beta1, rss), col=2)
+
+#Question 5
+galton_heights %>% ggplot(aes(father, son)) +
+  geom_point() +
+  geom_smooth(method = "lm")
+
+#Or alternatively:
+
+model <- lm(son ~ father, data = galton_heights)
+predictions <- predict(model, interval = c("confidence"), level = 0.95)
+data <- as.tibble(predictions) %>% bind_cols(father = galton_heights$father)
+
+ggplot(data, aes(x = father, y = fit)) +
+  geom_line(color = "blue", size = 1) + 
+  geom_ribbon(aes(ymin=lwr, ymax=upr), alpha=0.2) + 
+  geom_point(data = galton_heights, aes(x = father, y = son))
+
+#Question 7
+
+set.seed(1989, sample.kind="Rounding") #if you are using R 3.6 or later
+library(HistData)
+data("GaltonFamilies")
+options(digits = 3)    # report 3 significant digits
+
+female_heights <- GaltonFamilies %>%     
+  filter(gender == "female") %>%     
+  group_by(family) %>%     
+  sample_n(1) %>%     
+  ungroup() %>%     
+  select(mother, childHeight) %>%     
+  rename(daughter = childHeight)
+
+fit <-female_heights %>% lm(mother ~ daughter, data = .)
+
+#Question 8
+head(female_heights)
+predict(fit)[1]
+
